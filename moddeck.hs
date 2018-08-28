63bd222ed396ae97ba7f05e8a16d8cc6fe2fd308
magras <magras@users.noreply.github.com>
2018-08-28 19:47:07 +0300

    added kill chance tables

--- moddeck.hs
+++ moddeck.hs
@@ -1,11 +1,11 @@
 module GloomhavenModDeck where
 
+import Prelude hiding (Left, Right)
+import Control.Applicative (liftA)
 import Debug.Trace (trace, traceShow, traceShowId)
 import Text.Printf (printf)
 import Data.Ratio ((%))
-import Data.List (nub, sort)
-import Data.Monoid
-import Control.Applicative
+import Data.List (nub, sort, intercalate)
 import qualified Data.Map.Strict as Map
 
 type Count = Integer
@@ -15,6 +15,9 @@ type Card = String
 type Modifier = Damage -> Damage
 type Deck = [(Card, Count)]
 type DamageDistribution = [(Damage, Probability)]
+
+data AttackType = Normal | Advantage | Disadvantage deriving (Eq, Show)
+data Alignment = Left | Right | Center
 
 group :: (Ord key, Num val) => [(key, val)] -> [(key, val)]
 group = Map.toList . Map.fromListWith (+)
@@ -64,7 +67,8 @@ getModifier  "0" = const 0
 getModifier card = trace ("Unknown card: " ++ show card) undefined
 
 applyModifier :: Card -> Damage -> Damage
-applyModifier card = getModifier $ removeSpecialEffect $ removeRolling $ card
+applyModifier card = max 0 . modifier
+  where modifier = getModifier $ removeSpecialEffect $ removeRolling $ card
 
 compareCards :: Damage -> Card -> Card -> Ordering
 compareCards = compareCardsByOfficialRules
@@ -303,58 +307,70 @@ printAllDeckStats baseDmg = do
   printDeckVariantStats "Summoner" summonerDeck baseDmg
 
 cragheartDeck0 = [
-  ( "x2", 1),
-  ( "+2", 3),
-  ( "+1", 8),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 1),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 0),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
 
 cragheartDeck1 = [
-  ( "x2", 1),
-  ( "+2", 5),
-  ( "+1", 8),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 2),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 3),
+  ( "+1*", 0),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 2),
+  (  "0" , 1)]
 
 cragheartDeck2 = [
-  ( "x2", 1),
-  ( "+2", 3),
-  ( "+1", 9),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 1),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 1),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
 
 cragheartDeck3 = [
-  ( "x2", 1),
-  ( "+2", 5),
-  ( "+1", 9),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 2),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 3),
+  ( "+1*", 1),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 2),
+  (  "0" , 1)]
 
 cragheartDeck4 = [
-  ( "x2", 1),
-  ( "+2", 3),
-  ( "+1", 10),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 1),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 2),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
 
 cragheartDeck5 = [
-  ( "x2", 1),
-  ( "+2", 5),
-  ( "+1", 10),
-  ( "+0", 2),
-  ( "-1", 2),
-  ( "-2", 2),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 3),
+  ( "+1*", 2),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 2),
+  (  "0" , 1)]
 
 printCragheartDeckVariantStats :: Damage -> IO ()
 printCragheartDeckVariantStats baseDmg = do
@@ -378,38 +394,26 @@ printCragheartDeckVariantStats baseDmg = do
   putChar '\n'
 
 testDeck0 = [
-  ("r+2", 0),
-  ("r+1", 0),
-  ("r+0", 0),
-  ( "+4", 0),
-  ( "+3", 0),
-  ( "+2", 0),
-  ( "+1", 0),
-
-  ( "x2", 1),
-  ( "+2", 1),
-  ( "+1", 5),
-  ( "+0", 7),
-  ( "-1", 5),
-  ( "-2", 0),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 0),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
 
 testDeck1 = [
-  ("r+2", 0),
-  ("r+1", 2),
-  ("r+0", 0),
-  ( "+4", 0),
-  ( "+3", 0),
-  ( "+2", 0),
-  ( "+1", 0),
-
-  ( "x2", 1),
-  ( "+2", 1),
-  ( "+1", 5),
-  ( "+0", 6),
-  ( "-1", 5),
-  ( "-2", 1),
-  (  "0", 1)]
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 0),
+  ( "+1" , 8),
+  ( "+0" , 6),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
 
 printTestDeckStats :: Damage -> IO ()
 printTestDeckStats baseDmg = do
@@ -423,10 +427,144 @@ printTestDeckStats baseDmg = do
   printDeckStats testDeck1 baseDmg
   putChar '\n'
 
+
+tailDistribution :: DamageDistribution -> DamageDistribution
+tailDistribution =
+  scanr1 (\(d,p) (_,prob) -> (d, p + prob)) . sort
+
+killChanceTable :: Deck -> [Damage] -> [(AttackType, Damage, Damage, Probability)]
+killChanceTable deck baseDmgRange =
+  concat [killChances deck baseDmg atkType | baseDmg <- baseDmgRange, atkType <- [Normal, Advantage, Disadvantage]]
+  where
+    killChances :: Deck -> Damage -> AttackType -> [(AttackType, Damage, Damage, Probability)]
+    killChances deck baseDmg atkType =
+      map (\(resultDmg, prob) -> (atkType, baseDmg, resultDmg, prob))
+      $ tailDistribution $ (attack atkType) deck baseDmg
+
+    attack :: AttackType -> Deck -> Damage -> DamageDistribution
+    attack Normal = normalAttack
+    attack Advantage = attackWithAdvantage
+    attack Disadvantage = attackWithDisadvantage
+
+printKillChanceTable :: Deck -> [Damage] -> IO ()
+printKillChanceTable deck baseDmgRange = do
+  putStrLn $ text
+  where
+    dict :: [((AttackType, Damage, Damage), Probability)]
+    dict =
+      map (\(atk, bd, rd, p) -> ((atk,bd,rd),p)) $
+      killChanceTable deck baseDmgRange
+
+    table :: [[[Maybe Probability]]]
+    table = [[[lookup (atkType, baseDmg, resultDmg) dict
+      | atkType <- [Disadvantage, Normal, Advantage]]
+      | baseDmg <- baseDmgRange]
+      | resultDmg <- resultDmgRange]
+    
+    textTable :: [[String]]
+    textTable = map (map formatter) table
+      where
+        formatter :: [Maybe Probability] -> String
+        formatter = intercalate " / " . map showMaybeProbability
+
+    textTableWithHeaders :: [[String]]
+    textTableWithHeaders = addFirstColumn $ addHeader $ textTable
+
+    text :: String
+    text = intercalate "\n" $ map (intercalate " | ") $ textTableWithHeaders
+
+    addHeader :: [[String]] -> [[String]]
+    addHeader tbl = map (alignCenter 24 . show) baseDmgRange : tbl
+
+    addFirstColumn :: [[String]] -> [[String]]
+    addFirstColumn = zipWith (:) (map (alignRight 2) $ "" : map show resultDmgRange)
+
+    alignText :: Alignment -> Int -> String -> String
+    alignText algn width str = left ++ str ++ right
+      where
+        len = length str
+        padding = width - len
+        (leftPadding, rightPadding) = case algn of
+          Center -> ((padding + 1) `quot` 2, padding `quot` 2)
+          Left -> (0, padding)
+          Right -> (padding, 0)
+        left = replicate leftPadding ' '
+        right = replicate rightPadding ' '
+
+    alignCenter :: Int -> String -> String
+    alignCenter = alignText Center
+
+    alignRight :: Int -> String -> String
+    alignRight = alignText Right
+
+    showMaybeProbability :: Maybe Probability -> String
+    showMaybeProbability (Just p) = showProbability p
+    showMaybeProbability Nothing = "   -  "
+
+    showProbability :: Probability -> String
+    showProbability = printf "%6.2f" . toPercent
+
+    toPercent :: Probability -> Float
+    toPercent = fromRational . (*100)
+
+    resultDmgRange :: [Damage]
+    resultDmgRange = nub $ sort $ filter (/=0) $ map (\((_, _, resultDmg), _) -> resultDmg) dict
+
+printPartyKillChanceTables :: [Damage] -> IO ()
+printPartyKillChanceTables baseDmgRange = do
+  putStrLn "Base Deck"
+  printKillChanceTable baseDeck baseDmgRange
+  putChar '\n'
+  putStrLn "Ragerock"
+  printKillChanceTable ragerockDeck baseDmgRange
+  putChar '\n'
+  putStrLn "Солнышко"
+  printKillChanceTable sunDeck baseDmgRange
+  putChar '\n'
+  putStrLn "Vetoz"
+  printKillChanceTable vetozDeck baseDmgRange
+  putChar '\n'
+
+ragerockDeck = [
+  ( "x2" , 1),
+  ( "+2*", 2),
+  ( "+2" , 1),
+  ( "+1*", 0),
+  ( "+1" , 8),
+  ( "+0" , 2),
+  ( "-1" , 2),
+  ( "-2" , 1),
+  (  "0" , 1)]
+
+sunDeck = [
+  ("r+1" , 4),
+  ("r+0*", 3),
+  ( "x2" , 1),
+  ( "+2" , 2),
+  ( "+1" , 7),
+  ( "+0" , 6),
+  ( "-1" , 5),
+  ( "-2" , 0),
+  (  "0" , 1)]
+
+vetozDeck = [
+  ( "x2" , 1),
+  ( "+3" , 1),
+  ( "+2" , 1),
+  ( "+1*", 2),
+  ( "+1" , 7),
+  ( "+0" , 7),
+  ( "-1" , 5),
+  ( "-2" , 0),
+  (  "0" , 1)]
+
+
 main :: IO ()
 main = do
-  printAllDeckStats baseDmg
+  -- printAllDeckStats baseDmg
   -- printCragheartDeckVariantStats baseDmg
   -- printTestDeckStats baseDmg
+  printPartyKillChanceTables baseDmgRange
   where
     baseDmg = 3
+    baseDmgRange = [1..5]
